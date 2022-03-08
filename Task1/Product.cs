8773a4c6a857e957e2bc5066d484f070c1f2920e
Jakhongir7 <bakhodirovjakhongir@gmail.com>
2022-03-08 13:44:29 +0500

    Override Equals

--- Task1/Product.cs
+++ Task1/Product.cs
@@ -11,5 +11,18 @@
         public string Name { get; set; }
 
         public double Price { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as Product);
+        }
+
+        public bool Equals(Product productToFind)
+        {
+            if (productToFind == null)
+                return false;
+
+            return this.Name.Equals(productToFind.Name) && this.Price.Equals(productToFind.Price);
+        }
     }
 }
